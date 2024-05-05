6c8a416d0b5a8cdd8fdecae8fe7fb5ce05096cf6
YASINRA <yas.am.1390@gmail.com>
2024-05-05 20:20:55 +0330

    edit home.blade

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -10,7 +10,19 @@
     <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
         
         <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+            
+            {{-- Stories --}}
 
+            <section>
+                <ul class="flex overflow-x-auto items-center gap-2">
+                        @for ($i = 0; $i < 10; $i++)
+                        <li class="flex flex-col justify-center w-20 gap-1 p-2">
+                            <x-avatar story src="https://source.unsplash.com/500x500?face{{$i}}" class="h-14 w-14" />
+                            <p class="text-xs font-medium truncate"> {{fake()->name}}</p>
+                        </li>
+                        @endfor
+                </ul>
+            </section>
         </aside>
 
         {{-- sugestions --}}
