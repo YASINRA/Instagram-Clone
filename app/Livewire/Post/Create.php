db853cd8b1ccd0366fdbbc51bb9a721112f777c2
YASINRA <yas.am.1390@gmail.com>
2024-05-08 11:23:21 +0330

    Add properties and methods

--- app/Livewire/Post/Create.php
+++ app/Livewire/Post/Create.php
@@ -3,10 +3,32 @@
 namespace App\Livewire\Post;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
 class Create extends ModalComponent
 {
+    use WithFileUploads;
+
+    public $media=[];
+    public $description;
+    public $location;
+
+    public $hide_like_view;
+
+    public $allow_commenting;
+
+    function submit()
+    {
+
+        dd([
+            $this->media,
+            $this->description,
+            $this->location,
+            $this->hide_like_view,
+            $this->allow_commenting
+        ]);
+    }
 
     /**
      * Supported: 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl'
