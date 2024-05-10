91c510c1d86537bd47e23e0a99e78ba20ffe46e2
YASINRA <yas.am.1390@gmail.com>
2024-05-10 10:41:15 +0330

    Toggle Follow Users

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 use Livewire\Attributes\On;
@@ -54,6 +55,11 @@ class Home extends Component
         $this->canLoadMore = (count($this->posts) >= $this->perPage);
     }
 
+    function toggleFollow(User $user)  {
+        abort_unless(auth()->check(),401);
+        auth()->user()->toggleFollow($user);
+    }
+
     function mount()
     {
         $this->loadPosts();
@@ -61,6 +67,7 @@ class Home extends Component
 
     public function render()
     {
-        return view('livewire.home');
+       $suggestedUsers=  User::limit(5)->get();
+        return view('livewire.home',['suggestedUsers'=>$suggestedUsers]);
     }
 }
