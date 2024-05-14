511e58db33d25ce525a8504bd3298056a3d192a5
YASINRA <yas.am.1390@gmail.com>
2024-05-15 00:25:20 +0330

    Show Explore Posts

--- app/Livewire/Explore.php
+++ app/Livewire/Explore.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Explore extends Component
 {
+     #[On('closeModal')]
+     public function revertUrl()
+     {
+         $this->js("history.replaceState({}, '', '/explore')");
+     }
+
     public function render()
     {
-        return view('livewire.explore');
+        $posts= Post::limit(20)->get();
+        return view('livewire.explore',['posts'=>$posts]);
     }
 }
