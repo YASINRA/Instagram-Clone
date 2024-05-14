26699abeebdc75c59eed7a46def8f0e8b4c87318
YASINRA <yas.am.1390@gmail.com>
2024-05-14 23:53:49 +0330

    Display Users Posts

--- app/Livewire/Profile/Home.php
+++ app/Livewire/Profile/Home.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire\Profile;
 
+use App\Models\Post;
 use App\Models\User;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Home extends Component
@@ -11,20 +13,30 @@ class Home extends Component
 
     public $user;
 
-    function toggleFollow()  {
-        abort_unless(auth()->check(),401);
+    #!!!import use Livewire\Attributes\On;
+    #[On('closeModal')]
+    public function revertUrl()
+    {
+
+        $this->js("history.replaceState({}, '', '/')");
+    }
+    function toggleFollow()
+    {
+        abort_unless(auth()->check(), 401);
         auth()->user()->toggleFollow($this->user);
     }
 
-    function mount($user)  {
-        $this->user= User::whereUsername($user)->withCount(['followers','followings','posts'])->firstOrFail();
+    function mount($user)
+    {
+        $this->user = User::whereUsername($user)->withCount(['followers', 'followings', 'posts'])->firstOrFail();
     }
 
     public function render()
     {
         #add this in order to update the withCount() variables on hydrate
-        $this->user= User::whereUsername($this->user->username)->withCount(['followers','followings','posts'])->firstOrFail();
+        $this->user = User::whereUsername($this->user->username)->withCount(['followers', 'followings', 'posts'])->firstOrFail();
 
-        return view('livewire.profile.home');
+        $posts = $this->user->posts()->whereType('post')->get();
+        return view('livewire.profile.home', ['posts' => $posts]);
     }
 }
