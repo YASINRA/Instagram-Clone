9c6fa93290414d7efbcefc04e6b95ef0913727de
YASINRA <yas.am.1390@gmail.com>
2024-05-15 14:52:40 +0330

    Create Reels Component

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,19 @@ use App\Livewire\Profile\Home as ProfileHome;
 use App\Livewire\Profile\Reels;
 use App\Livewire\Profile\Saved;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Reels as livewireReels;
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+Route::middleware('auth')->group(function () {
+     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::middleware('auth')->group(function () {
     //Home
     Route::get('/', Home::class)->name('home');
     Route::get('/explore', Explore::class)->name('explore');
+    Route::get('/reels', livewireReels::class)->name('reels');
 
     //Profile
     Route::get('/profile/{user}', ProfileHome::class)->name('profile.home');
