56d354031594cfa7ab6b15b07b79fd06eb1fddc3
YASINRA <yas.am.1390@gmail.com>
2024-05-19 22:53:52 +0330

    Create PostLikedNotification

--- app/Livewire/Post/Item.php
+++ app/Livewire/Post/Item.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Post;
 
 use App\Models\Comment;
 use App\Models\Post;
+use App\Notifications\PostLikedNotification;
 use Livewire\Component;
 
 class Item extends Component
@@ -11,36 +12,44 @@ class Item extends Component
     public Post $post;
     public $body;
 
-    function togglePostLike()  {
-        abort_unless(auth()->check(),401);
+    function togglePostLike()
+    {
+        abort_unless(auth()->check(), 401);
         auth()->user()->toggleLike($this->post);
+        #send notification post is liked
+        if ($this->post->isLikedBy(auth()->user())) {
+            $this->post->user->notify(new PostLikedNotification(auth()->user(), $this->post));
+        }
     }
 
-    function toggleFavorite()  {
-        abort_unless(auth()->check(),401);
+    function toggleFavorite()
+    {
+        abort_unless(auth()->check(), 401);
         auth()->user()->toggleFavorite($this->post);
     }
 
-    function toggleCommentLike(Comment $comment)  {
-        abort_unless(auth()->check(),401);
+    function toggleCommentLike(Comment $comment)
+    {
+        abort_unless(auth()->check(), 401);
         auth()->user()->toggleLike($comment);
     }
 
-    function addComment()  {
-        $this->validate(['body'=>'required']);
+    function addComment()
+    {
+        $this->validate(['body' => 'required']);
         #create comment
         Comment::create([
-            'body'=>$this->body,
-            'commentable_id'=>$this->post->id,
-            'commentable_type'=>Post::class,
-            'user_id'=>auth()->id()
+            'body' => $this->body,
+            'commentable_id' => $this->post->id,
+            'commentable_type' => Post::class,
+            'user_id' => auth()->id()
         ]);
-        $this->reset( ['body']);
+        $this->reset(['body']);
     }
 
     public function render()
     {
-       // dd($this->post);
+        // dd($this->post);
         return view('livewire.post.item');
     }
 }
