0e3bf536c74f2fec1c842efc641fa9a87b2f8361
YASINRA <yas.am.1390@gmail.com>
2024-05-20 21:10:44 +0330

    Create Chat Index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Chat\Index;
 use App\Livewire\Explore;
 use App\Livewire\Home;
 use App\Livewire\Post\View\Page;
@@ -23,7 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/reels', LivewireReels::class)->name('reels');
 
     Route::get('/post/{post}', Page::class)->name('post');
-
+    Route::get('/chat', Index::class)->name('chat');
     //Profile
     Route::get('/profile/{user}', ProfileHome::class)->name('profile.home');
     Route::get('/profile/{user}/reels', Reels::class)->name('profile.reels');
