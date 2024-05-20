3ef674a99738fb8ae190529a2ffa4b191c12f8c0
YASINRA <yas.am.1390@gmail.com>
2024-05-20 21:14:49 +0330

    Create Chat list

--- app/Livewire/Chat/Index.php
+++ app/Livewire/Chat/Index.php
@@ -11,7 +11,7 @@ class Index extends Component
         return <<<'HTML'
                 <div class="w-full h-[calc(100vh_-_0.0rem)] flex bg-white    rounded-lg" >
                     <div class="  relative dark:border-gray-600 w-full h-full md:w-[320px] xl:w-[400px] border-r shrink-0 overflow-y-auto  ">
-                        <!-- <livewire:chat.chat-list> -->
+                        <livewire:chat.chat-list />
                     </div>
                     <main class=" hidden md:grid   w-full  dark:border-gray-700 h-full relative overflow-y-auto"  style="contain:content">
 
