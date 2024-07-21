f1651b81a45fdb1d14fbbaf148019e31c74fd980
YASINRA <yas.am.1390@gmail.com>
2024-07-22 01:32:05 +0330

    edit chat

--- app/Livewire/Chat/Chat.php
+++ app/Livewire/Chat/Chat.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace App\Livewire\Chat;
@@ -5,24 +6,25 @@ namespace App\Livewire\Chat;
 use App\Models\Conversation;
 use App\Models\Message;
 use App\Notifications\MessageSentNotification;
-use Livewire\Component;
 use Livewire\Attributes\On;
+use Livewire\Component;
 
 class Chat extends Component
 {
     public Conversation $conversation;
     public $receiver;
-    public $body = null;
+    public $body;
     public $loadedMessages;
     public $paginate_var = 10;
 
-    public function listenBroadcastedMessage($event)
+    function listenBroadcastedMessage($event)
     {
         $this->dispatch('scroll-bottom');
+
         $newMessage = Message::find($event['message_id']);
-        #push message
+
         $this->loadedMessages->push($newMessage);
-        #mark as read
+
         $newMessage->read_at = now();
         $newMessage->save();
     }
@@ -36,49 +38,48 @@ class Chat extends Component
             'receiver_id' => $this->receiver->id,
             'body' => $this->body
         ]);
-        #scroll to bottom
+
         $this->dispatch('scroll-bottom');
+
         $this->reset('body');
-        #push the message
+
         $this->loadedMessages->push($createdMessage);
-        #update conversation model -for sorting in chatlist
+
         $this->conversation->updated_at = now();
         $this->conversation->save();
-        #dispatch refresh event to chatlist
+
         $this->dispatch('refresh')->to(ChatList::class);
+
         $this->receiver->notify(new MessageSentNotification(
-            Auth()->User(),
+            auth()->user(),
             $createdMessage,
-            $this->conversation,
+            $this->conversation
         ));
     }
 
     #[On('loadMore')]
-    public function loadMore(): void
+    function loadMore()
     {
-        #increment
         $this->paginate_var += 10;
-        #call loadMessages()
         $this->loadMessages();
-        #update the chat height
         $this->dispatch('update-height');
     }
 
-    public function loadMessages()
+    function loadMessages()
     {
-        #get count
         $count = Message::where('conversation_id', $this->conversation->id)->count();
-        #skip and query
+
         $this->loadedMessages = Message::where('conversation_id', $this->conversation->id)
             ->skip($count - $this->paginate_var)
             ->take($this->paginate_var)
             ->get();
+
         return $this->loadedMessages;
     }
 
     function mount()
     {
-        $this->receiver =  $this->conversation->getReceiver();
+        $this->receiver = $this->conversation->getReceiver();
         $this->loadMessages();
     }
 
--- app/Livewire/Chat/Main.php
+++ app/Livewire/Chat/Main.php
@@ -14,7 +14,7 @@ class Main extends Component
     function mount()
     {
         $this->conversation = Conversation::findOrFail($this->chat);
-        #mark message belogning to receiver as read
+
         Message::where('conversation_id', $this->conversation->id)
             ->where('receiver_id', auth()->id())
             ->whereNull('read_at')
@@ -23,17 +23,17 @@ class Main extends Component
 
     public function render()
     {
-
         return <<<'HTML'
-                <div class="w-full h-[calc(100vh_-_0.0rem)]  flex  bg-white  rounded-lg" >
-                    <div class=" hidden lg:flex   relative w-full h-full md:w-[320px] xl:w-[400px] border-r shrink-0 overflow-y-auto  ">
-                        <livewire:chat.chat-list>
-                    </div>
+        <div class="w-full h-[calc(100vh_-_0.0rem)] flex bg-white rounded-lg">
 
-                    <main class="  grid  w-full   h-full relative overflow-y-auto"  style="contain:content">
-                      <livewire:chat.chat :conversation="$conversation">
-                    </main>
-                </div>
+            <div class=" hidden lg:flex  relative w-full h-full md:w-[320px]  xl:w-[400px] border-r shrink-0 overflow-y-auto">
+              <livewire:chat.chat-list />
+            </div>
+
+            <main class="grid w-full h-full relative overflow-y-auto" style="contain:content">
+              <livewire:chat.chat :conversation="$conversation" />
+            </main>
+        </div>
         HTML;
     }
 }
